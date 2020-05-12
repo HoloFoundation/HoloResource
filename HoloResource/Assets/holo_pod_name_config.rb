736807e372339b5ae5007fb653724907da920a9d
gonghonglou <gonghonglou@icloud.com>
2020-05-12 22:30:58 +0800

    ruby

--- HoloResource/Assets/holo_pod_name_config.rb
+++ HoloResource/Assets/holo_pod_name_config.rb
@@ -1,22 +1,12 @@
-##!/bin/sh
-#
-##basepath=$(cd `dirname $0`; pwd)
-##for file in $basepath/*
-##do
-## 	# 确定是目录
-## if test -d $file;then
-##     echo $file
-##     cd $file
-##     git pull
-## fi
-##done
-#
-#echo '---1'
-#echo ${TARGET_BUILD_DIR}
-#echo '---2'
-#echo ${UNLOCALIZED_RESOURCES_FOLDER_PATH}
-#echo '---3'
-#echo ${PODS_ROOT}
-#echo '---4'
-#echo ${RESOURCE_PATH}
-#echo '---5'
+
+# perform the method in Podfile
+def holo_config_pod_name
+    # or copy these code to Podfile
+    post_install do |installer|
+      installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = "'HOLO_POD_NAME=@(\"#{target}\")'"
+        end
+      end
+    end
+end
