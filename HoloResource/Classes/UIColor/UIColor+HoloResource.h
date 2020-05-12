c1343332906f55e9f03e1254c7380ef7e47aceb5
gonghonglou <gonghonglou@icloud.com>
2020-05-12 22:32:41 +0800

    add UIColor category

---
+++ HoloResource/Classes/UIColor/UIColor+HoloResource.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+HoloResource.h
+//  HoloResource
+//
+//  Created by 与佳期 on 2020/5/12.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (HoloResource)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HoloResource/Classes/UIColor/UIColor+HoloResource.m
@@ -0,0 +1,15 @@
+//
+//  UIColor+HoloResource.m
+//  HoloResource
+//
+//  Created by 与佳期 on 2020/5/12.
+//
+
+#import "UIColor+HoloResource.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation UIColor (HoloResource)
+
+@end
