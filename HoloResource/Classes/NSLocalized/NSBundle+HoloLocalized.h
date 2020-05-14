68eb6f2291d2da243d361d39fea59816b9721263
gonghonglou <gonghonglou@icloud.com>
2020-05-14 13:50:08 +0800

    HoloLocalizedStringWithDefaultValue

--- HoloResource/Classes/NSLocalized/NSBundle+HoloLocalized.h
+++ HoloResource/Classes/NSLocalized/NSBundle+HoloLocalized.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 #define HoloLocalizedStringFromTable(key, tbl, comment) \
         NSLocalizedStringFromTableInBundle(key, tbl, [NSBundle holo_bundleNamed:HOLO_POD_NAME], comment)
+#define HoloLocalizedStringWithDefaultValue(key, tbl, val, comment) \
+        NSLocalizedStringWithDefaultValue(key, tbl, [NSBundle holo_bundleNamed:HOLO_POD_NAME], val, comment)
 
 @interface NSBundle (HoloLocalized)
 
